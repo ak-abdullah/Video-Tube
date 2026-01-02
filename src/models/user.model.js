31edfa3c2bdbc611bd23ce96186895f67180d78a
Abdullah Khalid <abdullahkh.cs@gmail.com>
2026-01-02 15:24:24 +0500

    Some Changes Made

---
+++ src/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        index: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    coverImage: {
+        type: String,
+        required: true,
+    }
+    , watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Video',
+        },
+    ],
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refreshToken: {
+        type: String,
+    },
+}, { timestamps: true })
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+export const User = mongoose.model('User', userSchema)

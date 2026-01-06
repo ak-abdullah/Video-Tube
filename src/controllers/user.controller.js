92b8a98bcbf064db4a95da537b34346f51750e78
Abdullah Khalid <abdullahkh.cs@gmail.com>
2026-01-06 17:24:57 +0500

    Register Controller In Progress

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,47 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiError } from '../utils/ApiError.js'
+import { User } from '../models/user.model.js'
+import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import { upload } from '../middlewares/multer.middleware.js'
 
 const registerUser = asyncHandler(async (req, res) => {
 
-    return res.status(200).json({
+    const { name, email, password  } = req.body
+    if([name, email, password, username].some(field => field?.trim() === "")) {
+        throw new ApiError(400, 'All fields are required')
+    }
+    console.log(name, email, password)
 
-        message: 'ok',
+    const existedUser = User.findOne(
+        {
+            $or: [{username}, {email}]
+        }
+    )
+    if(existedUser) {
+        throw new ApiError(409, "User with name or username already exists")
+    }
+    
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar) {
+        throw new ApiError(400, "Failed to upload avatar to cloudinary")
+    }
+
+    User.create({
+        fullName: name,
+        email,
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        username: username.toLowerCase(),
     })
 })
 export { registerUser }
